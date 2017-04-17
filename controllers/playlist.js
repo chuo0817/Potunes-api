4ff39069e1cf949b9890678e93f2e44925461efe
Purchas <me@poche.fm>
2017-04-17 10:25:12 +0800

    update to iOS 1.2.7

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -39,7 +39,7 @@ export function *create(next) {
 
 
 export function* getPlaylists(next) {
-  if (this.query.v == '1.2.6' || (this.query.v == 'mina' && this.query.id == '1.0.1')) {
+  if (this.query.v == '1.2.7' || (this.query.v == 'mina' && this.query.id == '1.0.1')) {
     // return fake data
     const fakeLists = yield Playlists.getFake()
     return this.body = fakeLists
